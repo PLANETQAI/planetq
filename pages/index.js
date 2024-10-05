32c94fa4e9a904765a0b8075aadd474c1e697486
Hearts Antor <antorboss.bd@gmail.com>
2024-10-05 09:29:03 +0600

    make home page update

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,9 @@ import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+import Image from "next/image";
 
 export default function Home({ initialVideoLink }) {
-
-  const handleRedirect = (url) => {
-    window.location.href = url;
-};
-
   return (
     <>
       <Head>
@@ -20,32 +16,95 @@ export default function Home({ initialVideoLink }) {
         <link rel="icon" href="/images/small.webp" />
       </Head>
 
-      <div className="flex justify-center items-center">
-                <div className="video-container">
-                    <ReactPlayer
-                        url={'/images/final.mp4'}
-                        controls={false}
-                        loop={true}
-                        playing={true}
-                        volume={1}
-                        muted={true}
-                        className="react-player"
-                    />
-                    <div className="logo-container1">
-                        <img src="/images/V_left.gif" alt="Logo1" className="Vlogo" onClick={() => handleRedirect('https://planetqproductions.wixsite.com/planet-q-productions/faqs')} />
-                    </div>
-                    <div className="logo-container2">
-                        <img src="/images/V_center.jpg" alt="Logo2" className="Vlogo" onClick={() => handleRedirect('https://planetqproductions.wixsite.com/planet-q-productions')} />
-                    </div>
-                    <div className="logo-container3">
-                        <img src="/images/V_right.jpg" alt="Logo3" className="Vlogo" onClick={() => handleRedirect('/aistudio')} />
-                    </div>
+      <div className="flex flex-col min-h-screen bg-[#17101D]">
+        <div className="md:flex-grow flex justify-center items-center">
+          <div className="video-container">
+            <ReactPlayer
+              url={"/images/final.mp4"}
+              controls={false}
+              loop={true}
+              playing={true}
+              volume={1}
+              muted={true}
+              className="react-player"
+            />
+            <div className="logo-container1 group">
+              <div className="logo-container1-left">
+                <div className="logo-container1-left-text group-hover:animate-vibrate">
+                  <h1>Planet Q Radio</h1>
                 </div>
+                <button
+                  onClick={() =>
+                    handleRedirect(
+                      "https://planetqproductions.wixsite.com/planet-q-productions/faqs"
+                    )
+                  }
+                >
+                  <img src="/images/V_left.gif" alt="Logo1" className="Vlogo" />
+                </button>
+              </div>
             </div>
-            <div className="audio-container">
-                <AudioPlayer muted={false} autoPlay loop src="https://radio.planetqproductions.com/listen/planetq/radio.mp3" className="react-audio-player" volume={1.0}
-                 />
+            <div className="logo-container2 group">
+              <div className="logo-container1-left">
+                <div className="logo-container1-left-text group-hover:animate-vibrate">
+                  <h1>Planet Q Productions</h1>
+                </div>
+                <button
+                  onClick={() =>
+                    handleRedirect(
+                      "https://planetqproductions.wixsite.com/planet-q-productions"
+                    )
+                  }
+                >
+                  <img
+                    src="/images/V_center.jpg"
+                    alt="Logo1"
+                    className="Vlogo"
+                  />
+                </button>
+              </div>
             </div>
+            <div className="logo-container3 group">
+              <div className="logo-container1-left">
+                <div className="logo-container1-left-text group-hover:animate-vibrate">
+                  <h1>Q World Studios</h1>
+                </div>
+                <button onClick={() => handleRedirect("/aistudio")}>
+                  <img
+                    src="/images/V_right.jpg"
+                    alt="Logo1"
+                    className="Vlogo"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-gray-800" style={{ padding: "10px" }}>
+          <iframe
+            src="https://radio.planetqproductions.com/public/planetq/embed?theme=dark&autoplay=true" // Added autoplay parameter
+            frameBorder="0"
+            allowTransparency="true"
+            style={{
+              width: "100%",
+              minHeight: "130px",
+              border: "0",
+              maxHeight: "130px",
+            }}
+            title="Radio Planet Q"
+            allow="autoplay; encrypted-media" // Ensure autoplay is allowed
+          ></iframe>
+        </div>
+
+        <div className="banner-container block xl:hidden">
+          <img
+            src="/images/planetQproductions.jpg"
+            alt="Banner"
+            className="w-full h-auto object-cover"
+          />
+        </div>
+      </div>
     </>
   );
 }
